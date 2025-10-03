55d6c053316e4deb7e96ba20c507d2ed9cb8ebf6
parryk303 <kyle.parry.303@gmail.com>
2025-10-03 11:57:00 -0600

    Remove auth

--- app/page.js
+++ app/page.js
@@ -1,21 +1,403 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
+"use client";
 
-export default async function Landing() {
-    const supabase = createServerComponentClient({ cookies })
-    const { data: { session } } = await supabase.auth.getSession()
+import {
+  Button,
+  Container,
+  TextField,
+  Grid,
+  Typography,
+  Box,
+  Modal,
+  IconButton,
+  Divider,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-    if (session) {
-        redirect('/home')
+export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [components, setComponents] = useState("");
+  const [imports, setImports] = useState("");
+  const [icons, setIcons] = useState("");
+  const [hooks, setHooks] = useState("");
+  const [house, setHouse] = useState("");
+  const [sorted, setSorted] = useState();
+  const [get, setGet] = useState(false);
+
+  useEffect(() => {
+    const sortHouse = (house) => {
+      const result = {
+        Imports: imports
+          .trim()
+          .replace(/\n/g, "|")
+          .split("|")
+          .sort((a, b) => b.length - a.length)
+          .join("<>")
+          .replace(/<>/g, "\n")
+          .replace(/"/g, "'"),
+        Icons: icons
+          .trim()
+          .replace(/\n/g, ",")
+          .split(",")
+          .sort((a, b) => b.length - a.length)
+          .join()
+          .replace(/,/g, "\n")
+          .replace(/"/g, "'"),
+        Components: components
+          .trim()
+          .replace(/\n/g, ",")
+          .split(",")
+          .sort((a, b) => b.length - a.length)
+          .join()
+          .replace(/,/g, "\n")
+          .replace(/"/g, "'"),
+        Hooks: hooks
+          .trim()
+          .split(/\n+/)
+          .map((item) => {
+            return item.trim().replace(/"/g, "'");
+          })
+          .sort((a, b) => b.length - a.length)
+          .join("\n"),
+      };
+      return result[house];
+    };
+    if (get && house) {
+      setSorted(sortHouse(house));
+      setIsModalOpen(true);
+      setComponents("");
+      setImports("");
+      setIcons("");
+      setHooks("");
+      setGet(false);
+    } else {
+      setGet(false);
     }
+  }, [get, isModalOpen, house, sorted]);
 
-    if (!session) {
-        redirect('/login')
-    }
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-    return (
-        <p>Please sign in</p>
-    )
+  const handleSort = (e) => {
+    const id = e.currentTarget.id;
+    const hogwarts = {
+      Gryffindor: "Imports",
+      Slytherin: "Icons",
+      Hufflepuff: "Components",
+      Ravenclaw: "Hooks",
+    };
+    setHouse(hogwarts[id]);
+    setGet(true);
+  };
 
+  const modalContent = (
+    <Box
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 900,
+        bgcolor: "background.paper",
+        boxShadow: 24,
+        p: 4,
+      }}
+    >
+      <IconButton
+        sx={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+        }}
+        onClick={closeModal}
+      >
+        <CloseIcon />
+      </IconButton>
+      <Box sx={{ display: "flex" }}>
+        <Image
+          width={200}
+          height={200}
+          alt="sorting hat"
+          src="/sortinghat.jpg"
+        />
+        <Typography
+          variant="h4"
+          sx={{ fontFamily: "AlmendraSC", marginTop: "75px" }}
+          gutterBottom
+        >
+          Your {house} have been sorted!
+        </Typography>
+      </Box>
+
+      {house && sorted && (
+        <Typography
+          sx={{
+            whiteSpace: "pre-line",
+            fontFamily: "monospace",
+            color: "green",
+          }}
+        >
+          {sorted}
+        </Typography>
+      )}
+    </Box>
+  );
+
+  const greatHall =
+    "url(https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/hostedimages/1398089831i/9366155._SX540_.jpg)";
+
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Typography variant="h2" align="center" sx={{ fontFamily: "AlmendraSC" }}>
+        Sorting Hat
+      </Typography>
+      <Container
+        sx={{
+          backgroundImage: greatHall,
+          backgroundSize: "65% auto", 
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <Grid container spacing={3}>
+          {/* IMPORTS */}
+          <Grid
+            sx={{
+              display: "grid",
+              position: "relative",
+              left: "-150px",
+            }}
+            item
+            xs={12}
+            md={6}
+          >
+            <Typography
+              variant="h4"
+              sx={{ fontFamily: "MedievalSharp", marginLeft: "90px" }}
+            >
+              Imports
+            </Typography>
+            <TextField
+              disablezoom="true"
+              autoComplete="off"
+              sx={{ width: "auto" }}
+              style={{ width: "300px" }}
+              margin="normal"
+              multiline={true}
+              minRows={10}
+              maxRows={10}
+              size="medium"
+              value={imports}
+              onChange={(e) => setImports(e.target.value)}
+            />
+            <Button
+              id="Gryffindor"
+              sx={{
+                position: "relative",
+                top: "7px",
+                width: "70px",
+                left: "120px",
+              }}
+              variant="contained"
+              color="primary"
+              onClick={handleSort}
+            >
+              Sort
+            </Button>
+          </Grid>
+
+          {/* ICONS */}
+          <Grid
+            sx={{
+              display: "grid",
+              position: "relative",
+              right: "-425px",
+            }}
+            item
+            xs={12}
+            md={6}
+          >
+            <Typography
+              variant="h4"
+              sx={{ fontFamily: "MedievalSharp", marginLeft: "110px" }}
+            >
+              Icons
+            </Typography>
+            <TextField
+              disablezoom="true"
+              autoComplete="off"
+              sx={{ width: "auto" }}
+              style={{ width: "300px" }}
+              margin="normal"
+              multiline={true}
+              minRows={10}
+              maxRows={10}
+              size="medium"
+              value={icons}
+              onChange={(e) => setIcons(e.target.value)}
+            />
+            <Button
+              id="Slytherin"
+              sx={{
+                position: "relative",
+                top: "7px",
+                width: "70px",
+                left: "120px",
+              }}
+              variant="contained"
+              color="primary"
+              onClick={handleSort}
+            >
+              Sort
+            </Button>
+          </Grid>
+
+          {/* COMPONENTS */}
+          <Grid
+            sx={{
+              display: "grid",
+              position: "relative",
+              left: "-150px",
+            }}
+            item
+            xs={12}
+            md={6}
+          >
+            <Typography
+              variant="h4"
+              sx={{ fontFamily: "MedievalSharp", marginLeft: "55px" }}
+            >
+              Components
+            </Typography>
+            <TextField
+              disablezoom="true"
+              autoComplete="off"
+              sx={{ width: "auto" }}
+              style={{ width: "300px" }}
+              margin="normal"
+              multiline={true}
+              minRows={10}
+              maxRows={10}
+              size="medium"
+              value={components}
+              onChange={(e) => setComponents(e.target.value)}
+            />
+            <Button
+              id="Hufflepuff"
+              sx={{
+                position: "relative",
+                top: "7px",
+                width: "70px",
+                left: "120px",
+              }}
+              variant="contained"
+              color="primary"
+              onClick={handleSort}
+            >
+              Sort
+            </Button>
+          </Grid>
+
+          {/* HOOKS */}
+          <Grid
+            sx={{
+              display: "grid",
+              position: "relative",
+              right: "-425px",
+            }}
+            item
+            xs={12}
+            md={6}
+          >
+            <Typography
+              variant="h4"
+              sx={{ fontFamily: "MedievalSharp", marginLeft: "110px" }}
+            >
+              Hooks
+            </Typography>
+            <TextField
+              disablezoom="true"
+              autoComplete="off"
+              sx={{ width: "auto" }}
+              style={{ width: "300px" }}
+              margin="normal"
+              multiline={true}
+              minRows={10}
+              maxRows={10}
+              size="medium"
+              value={hooks}
+              onChange={(e) => setHooks(e.target.value)}
+            />
+            <Button
+              id="Ravenclaw"
+              sx={{
+                position: "relative",
+                top: "7px",
+                width: "70px",
+                left: "120px",
+              }}
+              variant="contained"
+              color="primary"
+              onClick={handleSort}
+            >
+              Sort
+            </Button>
+          </Grid>
+
+        </Grid>
+      </Container>
+
+      {isModalOpen && (
+        <Modal open={isModalOpen} onClose={closeModal}>
+          {modalContent}
+        </Modal>
+      )}
+
+      <Link href="/spells" passHref>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "20px",
+            right: "20px",
+          }}
+        >
+          <Image
+            width={70}
+            height={70}
+            alt="spell book"
+            src="/images/spellbook.png"
+          />
+        </Box>
+      </Link>
+
+      <Link href="/sorting" passHref>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "20px",
+            left: "20px",
+          }}
+        >
+          <Image
+            width={70}
+            height={70}
+            alt="spell book"
+            src="/images/cat.jpg"
+          />
+        </Box>
+      </Link>
+    </Box>
+  );
 }
